420a695965a6495fd76ba01fceb12e4eda984848
Nick Battjes <nhbattjes@gmail.com>
2018-08-07 20:30:36 -0700

    bootstrapping user, hopefully I don't break everything again

--- Resaled/app/controllers/application_controller.rb
+++ Resaled/app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in
 
   def login(user)
     user.reset_session_token
