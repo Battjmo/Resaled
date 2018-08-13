0e4493eda389720180798003c19373f325e21f43
Nick Battjes <nhbattjes@gmail.com>
2018-08-12 19:21:25 -0700

    getting all images to work on heroku, take 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.serve_static_files = true
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
