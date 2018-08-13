712fcc1558e8e7a4ed3236d131b59a867ae19e95
Nick Battjes <nhbattjes@gmail.com>
2018-08-12 20:25:29 -0700

    Clean up listing CSS a little

--- frontend/components/listings/listings_index_item.jsx
+++ frontend/components/listings/listings_index_item.jsx
@@ -3,8 +3,8 @@ import { Link, withRouter } from 'react-router-dom';
 
 const ListingsIndexItem = (props) => {
   return (
+    <Link className="listing-link" to={`/api/listings/${props.id}`}>
     <li className="index-listing" id={props.id}>
-      <Link className="listing-link" to={`/api/listings/${props.id}`}>
       <img className="listing-index-image" src={props.photo}/>
       <div className="listing-item-top-row">
         <h3 className="listing-item-brand">{props.listing.brand}</h3>
@@ -12,8 +12,8 @@ const ListingsIndexItem = (props) => {
         </div>
         <div className="listing-item-title">{props.listing.title}</div>
         <div className="listing-item-price">$ {props.listing.price}</div>
-      </Link>
     </li>
+          </Link>
   );
 };
 
