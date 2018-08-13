cddc53492283ce7b6e0c7305e81b32f547f1c15f
Nick Battjes <nhbattjes@gmail.com>
2018-08-12 19:31:06 -0700

    take whatever-we're-on-now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.serve_static_files = true
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
